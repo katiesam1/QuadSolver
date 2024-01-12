4ec3d61502b50a105067e8165fc2136ae2b6149c
Katie Sam <ksam25@students.polytechnic.org>
2024-01-12 14:33:19 -0800

    added description of quad formula

--- QuadraticSolver.java
+++ QuadraticSolver.java
@@ -2,5 +2,13 @@ public class QuadraticSolver{
 
     public static void main(String[] args){
         System.out.println("Welcome to the quadratic solver!!!!")
+        System.out.println("The Quadratic formula is ( -b +- sqrt(b^2-4ac))/2a")
     }
 }
+
+// making an update on development
+// add fields
+// commit with message
+// publish as github
+// switch to main
+// merge development to ain 
